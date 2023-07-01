e7fff67638b68f86c3f63315ef92973b82cfd5bd
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-07-01 18:21:55 +0200

    [add] component for contact form

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,47 @@
+
+<script>
+export default {
+  data(){
+    return{
+      name : '',
+      lastname : '',
+      message : '',
+    }
+  },
+
+  methods : {
+    sendForm(){
+      console.log('invio form');
+    }
+  }
+}
+</script>
+
+<template>
+<div class="container">
+  
+    <form @submit.prevent="sendForm">
+        
+        <div class="mb-3">
+          <label for="name" class="form-label">Nome</label>
+          <input type="text" class="form-control" id="name" placeholder="Inserisci nome" v-model="name">
+        </div>
+        
+        <div class="mb-5">
+          <label for="lastname" class="form-label">Cognome</label>
+          <input type="text" class="form-control" id="lastname" placeholder="Inserisci cognome" v-model="lastname">
+        </div>
+
+        <div class="form-floating mb-3">
+          <label for="message" class="text-secondary">Aggiungi un messaggio</label>
+          <textarea class="form-control" placeholder="Leave a comment here" id="message" v-model="message"></textarea>
+        </div>
+        
+        <button type="submit" class="btn btn-secondary">Submit</button>
+    </form>
+</div> 
+</template>
+
+<style>
+
+</style>
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,12 +1,23 @@
 
 <script>
+
+  import ContactForm from '../components/ContactForm.vue';
+
 export default {
-  name: 'Contacts'
+  name: 'Contacts',
+
+  components :{
+    ContactForm
+  }
 }
 </script>
 
 <template>
   <h1>Contacts</h1>
+
+  <ContactForm />
+
+
 </template>
 
 <style>
