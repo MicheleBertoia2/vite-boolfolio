688bf219dd326c661545ee0657f8748945e7218a
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-07-02 18:07:42 +0200

    [add] axios call for sending form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,17 +1,38 @@
 
 <script>
+  import axios from 'axios';
 export default {
   data(){
     return{
       name : '',
       lastname : '',
       message : '',
+      errors: {},
+      sending: false,
+      success: false
     }
   },
 
   methods : {
     sendForm(){
-      console.log('invio form');
+      this.sending = true;
+            const data = {
+                name: this.name,
+                lastname: this.lastname,
+                message: this.message,
+            }
+
+            axios.post('http://127.0.0.1:8000' + '/api/contacts', data)
+                .then(result => {
+                    this.sending = false;
+                    this.success = result.data.success;
+                    console.log(this.success, result.data);
+                    if(!result.data.success){
+                        this.errors = result.data.errors;
+                    }else{
+                        this.errors = {};
+                    }
+                })
     }
   }
 }
@@ -20,7 +41,7 @@ export default {
 <template>
 <div class="container">
   
-    <form @submit.prevent="sendForm">
+    <form v-if="!success" @submit.prevent="sendForm">
         
         <div class="mb-3">
           <label for="name" class="form-label">Nome</label>
@@ -33,12 +54,14 @@ export default {
         </div>
 
         <div class="form-floating mb-3">
-          <label for="message" class="text-secondary">Aggiungi un messaggio</label>
+          <span>Inserisci un messaggio</span>
           <textarea class="form-control" placeholder="Leave a comment here" id="message" v-model="message"></textarea>
         </div>
         
         <button type="submit" class="btn btn-secondary">Submit</button>
     </form>
+
+    <div v-else><h2 class="success">Form inviato correttamente</h2></div>
 </div> 
 </template>
 
